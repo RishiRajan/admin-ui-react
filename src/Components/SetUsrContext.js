17bf2698118c372ca6355e617480d8a4b2cb12ce
RishiRajan <mailrishirajan@gmail.com>
2023-04-24 01:05:06 +0530

    Update SetUsrContext.js

--- src/Components/SetUsrContext.js
+++ src/Components/SetUsrContext.js
@@ -3,7 +3,6 @@ import { createContext, useState } from "react";
 let UserContext = createContext(); // Store the Data
 
 export const UserProvider = ({ children }) => {
-  // Provide the Data
 
   const [user, setUser] = useState({ name: "" });
 
